22c1f3b8e14c91a35b98e95016e35fdd2e66f632
jw-macpro <wsklxsx@126.com>
2019-02-21 14:14:34 -0600

    update the function for the sql string parsing

--- DiffFilter/parse_sql.rb
+++ DiffFilter/parse_sql.rb
@@ -6,9 +6,13 @@ def parse_sql(ast)
 		if string_literal_node.type.to_s == "string_literal"
 			query_node = string_literal_node[0]
 			if query_node.type.to_s == "string_content"
-				
+				sql = query_node.source
+				parse_sql_string(sql)
 			end
 		end
 	end
+end
+# use pg query to handle sql query string
+def parse_sql_string(sql)
 end
 
