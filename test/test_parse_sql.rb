8290c9b6b92ac7cd22d27530727f00ed11708126
jw-macpro <wsklxsx@126.com>
2019-02-21 16:41:17 -0600

    fulfill the test case

--- test/test_parse_sql.rb
+++ test/test_parse_sql.rb
@@ -1,5 +1,11 @@
-load '../parse_sql.rb'
+require File.join(File.expand_path(File.dirname(__FILE__)), '../constraint_analyzer/parse_sql.rb')
+require "test/unit"
 require 'yard'
 
-contents = 'execute "UPDATE topics SET highest_staff_post_number = highest_post_number"'
-ast = YARD::Parser::Ruby::RubyParser.parse(contents).root
+class TestParseSql < Test::Unit::TestCase
+  def test_parse_sql
+  	contents = "execute \"UPDATE topics SET highest_staff_post_number = highest_post_number\"" 
+  	ast = YARD::Parser::Ruby::RubyParser.parse(contents).root
+    assert_equal ['topics', ['highest_staff_post_number']], parse_sql(ast[0][1])
+  end
+end
