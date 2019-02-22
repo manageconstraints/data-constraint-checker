7ce92ffdbf9446387e5b99fd71e6415a87c0ff9a
jw-macpro <wsklxsx@126.com>
2019-02-21 23:19:35 -0600

    test case for parse sql for query with <<-

--- constraint_analyzer/parse_sql.rb
+++ constraint_analyzer/parse_sql.rb
@@ -1,11 +1,11 @@
 require 'pg_query'
 require 'yard'
 def parse_sql(ast)
-	if ast.type.to_s == "list"
+	if ast&.type.to_s == "list"
 		string_literal_node = ast[0]
 		if string_literal_node.type.to_s == "string_literal"
 			query_node = string_literal_node[0]
-			if query_node.type.to_s == "string_content"
+			if query_node&.type.to_s == "string_content"
 				sql = query_node.source
 				if sql.start_with?"<-"
 					sql = handle_cross_line_string(sql)
