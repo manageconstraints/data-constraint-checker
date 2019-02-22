485a207f06da14824661342bcfe3446905826018
junwen yang <wsklxsx@126.com>
2019-02-22 21:51:06 +0000

    change argv[1] to argv[0]

--- constraint_analyzer/main.rb
+++ constraint_analyzer/main.rb
@@ -7,8 +7,8 @@ load 'version.rb'
 load 'extract_statistics.rb'
 application_dir = "/Users/jwy/Research/lobsters-ori/"
 load_validate_api
-if ARGV[1]
-	application_dir = ARGV[1]
+if ARGV[0]
+	application_dir = ARGV[0]
 	puts "application_dir #{application_dir}"
 end
 traverse_all_versions(application_dir, 1)
