24b17aa709e879cdaef3bc5336bc8729a28a72c4
jw-macpro <wsklxsx@126.com>
2019-02-23 22:16:05 -0600

    add checking before using versions

--- constraint_analyzer/extract_statistics.rb
+++ constraint_analyzer/extract_statistics.rb
@@ -16,13 +16,14 @@ def extract_commits(directory, interval=5)
 		if i % interval == 0
 			version = Version.new(directory, commit)
 			#version.build
-			versions << version
+			versions << version if version
 		end
 		i += 1
 	end
 	return versions
 end
 def traverse_all_versions(application_dir, interval)
+	return if versions.length <= 0
 	versions = extract_commits(application_dir, interval)
 	app_name = application_dir.split("/")[-1]
 	versions[0].build
@@ -31,35 +32,33 @@ def traverse_all_versions(application_dir, interval)
 	cnt = 0
 	for i in 1...versions.length
 		#puts "=============#{i}============="
-		begin
-			new_version = versions[i-1]
-			version = versions[i]
-			version.build
-			ncs, ccs = new_version.compare_constraints(version)
-			if ncs.length > 0 or ccs.length > 0
-				cnt += 1
-			end
-			model_ncs = ncs.select{|x| x.type == 'validate'}
-			db_ncs = ncs.select{|x| x.type == "db"}
-			model_ccs = ccs.select{|x| x.type == 'validate'}
-			db_ccs = ccs.select{|x| x.type == "db"}
-			c1 = model_ncs.length
-			c2 = db_ncs.length
-			c3 = model_ccs.length
-			c4 = db_ccs.length
-			output.write("#{ncs.length} #{c1} #{c2} #{ccs.length} #{c3} #{c4}\n")
-			ncs.each do|nc|
-				#puts "****New****"
-				#nc.self_print
-			end
-
-			ccs.each do|nc|
-				#puts "****DIFF****"
-				#nc.self_print
-			end
-			versions[i-1] = nil
-		rescue 
+		new_version = versions[i-1]
+		version = versions[i]
+		version.build
+		ncs, ccs = new_version.compare_constraints(version)
+		if ncs.length > 0 or ccs.length > 0
+			cnt += 1
 		end
+		model_ncs = ncs.select{|x| x.type == 'validate'}
+		db_ncs = ncs.select{|x| x.type == "db"}
+		model_ccs = ccs.select{|x| x.type == 'validate'}
+		db_ccs = ccs.select{|x| x.type == "db"}
+		c1 = model_ncs.length
+		c2 = db_ncs.length
+		c3 = model_ccs.length
+		c4 = db_ccs.length
+		output.write("#{ncs.length} #{c1} #{c2} #{ccs.length} #{c3} #{c4}\n")
+		ncs.each do|nc|
+			#puts "****New****"
+			#nc.self_print
+		end
+
+		ccs.each do|nc|
+			#puts "****DIFF****"
+			#nc.self_print
+		end
+		versions[i-1] = nil
+		
 	end
 	output.write("cnt: #{cnt}\n")
 	output.close
