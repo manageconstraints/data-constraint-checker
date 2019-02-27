d490b365323914604dd8f1683ade8f78bf92ebcb
jw-macpro <wsklxsx@126.com>
2019-02-27 00:05:51 -0600

    make the # of end less'

--- constraint_analyzer/parse_db_constraint.rb
+++ constraint_analyzer/parse_db_constraint.rb
@@ -76,10 +76,7 @@ def handle_change_column(ast)
 		table_class = $model_classes[class_name]
 		column = Column.new(table_class, column_name, column_type, $cur_class)
 		columns = table_class.getColumns
-		if columns[column_name]
-			# existing columns
-			column.prev_column =  columns[column_name]
-		end
+		column.prev_column =  columns[column_name]
 		table_class.addColumn(column)
 		# puts"create new column #{column.class.name} #{table_class.class_name} #{column_name} #{column_type}"
 		constraints = create_constraints(class_name, column_name, column_type, "db", dic)
@@ -103,40 +100,38 @@ def handle_create_table(ast)
 		end
 		columns = table_class.getColumns
 		## puts"table_name: #{table_name}"
-		if ast[2].type.to_s == "do_block"
-			ast[2].children.each do |child|
-				if child.type.to_s == 'list'
-					child.children.each do |c|
-						if c.type.to_s == "command_call"
-							column_type = c[2].source
-							if column_type == "references"
-								column_type = "string"
-							end
-							column_ast = c[3]
-							# puts"column_ast: #{column_ast.class}"
-							if column_ast.class.name == "YARD::Parser::Ruby::AstNode" and  column_ast.type.to_s == "list"
-								column_name = handle_symbol_literal_node(column_ast[0])
-								column = Column.new(table_class, column_name, column_type, $cur_class)
-								if columns[column_name]
-									# existing columns
-									column.prev_column =  columns[column_name]
+		return unless ast[2].type.to_s == "do_block"
+		ast[2].children.each do |child|
+			next unless child.type.to_s == 'list'
+			child.children.each do |c|
+				if c.type.to_s == "command_call"
+					column_type = c[2].source
+					if column_type == "references"
+						column_type = "string"
+					end
+					column_ast = c[3]
+					# puts"column_ast: #{column_ast.class}"
+					if column_ast.class.name == "YARD::Parser::Ruby::AstNode" and  column_ast.type.to_s == "list"
+						column_name = handle_symbol_literal_node(column_ast[0])
+						column = Column.new(table_class, column_name, column_type, $cur_class)
+						if columns[column_name]
+							# existing columns
+							column.prev_column =  columns[column_name]
+						end
+						table_class.addColumn(column)
+						dic = {}
+						if column_ast[1].class.name == "YARD::Parser::Ruby::AstNode" and column_ast[1].type and column_ast[1].type.to_s == "list"
+							column_ast[1].children.each do |cc|
+								if cc.type.to_s == "assoc"
+									key, value = handle_assoc_node(cc)
+									dic[key] = value
 								end
-								table_class.addColumn(column)
-								dic = {}
-								if column_ast[1].class.name == "YARD::Parser::Ruby::AstNode" and column_ast[1].type and column_ast[1].type.to_s == "list"
-									column_ast[1].children.each do |cc|
-										if cc.type.to_s == "assoc"
-											key, value = handle_assoc_node(cc)
-											dic[key] = value
-										end
-									end
-								end
-								# puts"-----------dic---------"
-								# putsdic
-								constraints = create_constraints(class_name, column_name, column_type, "db", dic)
-								table_class.addConstraints(constraints)
 							end
 						end
+						# puts"-----------dic---------"
+						# putsdic
+						constraints = create_constraints(class_name, column_name, column_type, "db", dic)
+						table_class.addConstraints(constraints)
 					end
 				end
 			end
