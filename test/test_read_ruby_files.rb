939c2c696f4aabbfe416711870f94c210df871b0
jw-macpro <wsklxsx@126.com>
2019-04-17 14:05:28 -0500

    match the html table to model

--- test/test_read_ruby_files.rb
+++ test/test_read_ruby_files.rb
@@ -55,5 +55,26 @@ class TestHTMLConstraint < Test::Unit::TestCase
     read_html_file_ast([test_filename])
     assert_equal table_class.getConstraints.size, 1
   end
+  def test_parse_haml_file2
+    puts "test_parse_haml_file2"
+    application_dir = File.join(File.expand_path(File.dirname(__FILE__)), 'haml_file2')
+    test_filename = application_dir + "/app/views/registrations/_form.haml"
+    load_html_constraint_api
+    table_class = Class_class.new("app/views/registrations/_form.haml")
+    table_class.class_name = "User"
+    column = Column.new(table_class, 'username', 'string', nil, {})
+    table_class.addColumn(column)
+    column2 = Column.new(table_class, 'password', 'string', nil, {})
+    table_class.addColumn(column2)
+    column3 = Column.new(table_class, 'password_confirmation', 'string', nil, {})
+    table_class.addColumn(column3)
+    $model_classes = {}
+    $model_classes["User"] = table_class
+    read_html_file_ast([test_filename])
+    assert_equal table_class.getConstraints.size, 3
+    assert_equal table_class.getConstraints.values[0].with_format, "[A-Za-z0-9_]+"
+    assert_equal table_class.getConstraints.values[1].with_format, "......+"
+    assert_equal table_class.getConstraints.values[2].with_format, "......+"
+  end
 
 end
