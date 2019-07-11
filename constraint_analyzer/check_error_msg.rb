638452663e82dab61835972a5968711cfb16ee04
Utsav Sethi <utsavsethi@gmail.com>
2019-07-10 19:13:42 -0500

    Add error message checking script

--- constraint_analyzer/check_error_msg.rb
+++ constraint_analyzer/check_error_msg.rb
@@ -2,22 +2,24 @@ require 'pathname'
 require 'yard'
 require 'pp'
 
-def check_all_apps()
-  # %w(discourse
-  # lobsters
-  # gitlabhq
-  # redmine
-  # spree
-  # ror_ecommerce
-  # fulcrum
-  # tracks
-  # onebody
-  # diaspora
-  # falling-fruit
-  # openstreetmap-website).each do |app_name|
-  #   check_custom_validations_for_error "/Users/utsavsethi/workspace/apps/" + app_name
-  # end
-  check_custom_validations_for_error "/Users/utsavsethi/workspace/apps/tracks"
+$error_results
+
+def check_all_apps(app_dir)
+  $error_results = {}
+  %w(discourse
+  lobsters
+  gitlabhq
+  redmine
+  spree
+  ror_ecommerce
+  fulcrum
+  tracks
+  onebody
+  diaspora
+  falling-fruit
+  openstreetmap-website).each do |app_name|
+    check_app_for_missing_errors(app_dir + app_name, app_name)
+  end
 
 end
 
@@ -34,11 +36,17 @@ def os_walk(dir)
   [root, files, dirs]
 end
 
-def check_custom_validations_for_error(application_dir=nil)
+def check_app_for_missing_errors(application_dir=nil, app_name)
   unless application_dir
     puts "application dir not defined"
     return
   end
+
+  $cur_app = app_name
+
+  $error_results[app_name] = {}
+  $error_results[app_name][:missing] = {}
+  $error_results[app_name][:found] = {}
 
   root, files, dirs = os_walk(application_dir)
   model_files = []
@@ -49,75 +57,145 @@ def check_custom_validations_for_error(application_dir=nil)
     end
   end
 
+  #model_files = ["/Users/utsavsethi/workspace/apps/falling-fruit/app/models/observation.rb"]
   model_files.each do |filename|
     file = open(filename)
     contents = file.read
     file.close
     begin
+      $cur_file = filename
       ast = YARD::Parser::Ruby::RubyParser.parse(contents).root
-      parse_model_file_for_errors(ast)
+      search_ast_for_missing_errors(ast)
     rescue StandardError => e
-      puts "failed filename: #{filename}"
-      puts e.message
+      #puts "failed filename: #{filename}"
+      #puts e.message
     end
   end
 end
 
-def parse_model_file_for_errors(ast)
+def get_validation_function_names(ast)
   if ast.type.to_s == 'list'
     ast.children.each do |child|
-      parse_model_file_for_errors(child)
+      get_validation_function_names(child)
     end
   end
   if ast.type.to_s == 'module'
     if ast[1] and ast[1].type.to_s == "list"
       ast[1].each do |child|
-        parse_model_file_for_errors(child)
+        get_validation_function_names(child)
       end
     end
   end
   if ast.type.to_s == 'class'
-    c1 =  ast.children[0]
-    c2 = ast.children[1]
-    if c1 and c1.type.to_s == 'const_ref'
-      #puts "class #{c1.source}"
-    end
-    if c2 and (c2.type.to_s == 'var_ref' or c2.type.to_s == 'const_path_ref')
-      #puts "c2.source #{c2.source}"
-    end
-    # puts"filename: #{$cur_class.filename} "
-    # puts"classname: #{$cur_class.class_name} upper_class_name: #{$cur_class.upper_class_name}"
-    c3 = ast.children[2]
+    c1 =  ast.children[0] # Class name
+    c2 = ast.children[1] # Parent class name
+    c3 = ast.children[2] # Class definition
     if c3
-      parse_model_file_for_errors(c3)
+      get_validation_function_names(c3)
     end
   end
   if ast.type.to_s == "command"
     funcname = ast[0].source
     if %w(validate validates_each validate_with).include?funcname
-      #puts"funcname #{funcname} #{ast.source}"
-      check_validation_function(funcname, ast)
+      if ast[1].type == :list
+        ast[1].each do |name|
+          if name
+            $func_name_acc << name[0][0][0]
+          end
+        end
+      end
+    end
+  end
+end
+
+def found_missing(app_name, file, source)
+  $error_results[app_name][:missing][file] =  source
+end
+
+def did_not_find(app_name, file)
+  if $error_results[app_name][:found][file].nil?
+    $error_results[app_name][:found][file] = 1
+  else
+    $error_results[app_name][:found][file] = $error_results[app_name][:found][file] + 1
+  end
+end
+
+def check_for_error(ast, funcnames)
+  if ast.type.to_s == 'list'
+    ast.children.each do |child|
+      check_for_error(child, funcnames)
+    end
+  end
+  if ast.type.to_s == 'module'
+    if ast[1] and ast[1].type.to_s == "list"
+      ast[1].each do |child|
+        check_for_error(child, funcnames)
+      end
+    end
+  end
+  if ast.type.to_s == 'class'
+    c1 =  ast.children[0] # Class name
+    c2 = ast.children[1] # Parent class name
+    c3 = ast.children[2] # Class definition
+    if c3
+      check_for_error(c3, funcnames)
     end
   end
   if ast.type.to_s == "fcall"
     funcname = ast[0].source
-    if %w(validate validates_each validate_with).include?funcname
-      #puts"funcname #{funcname} #{ast.source}"
-      check_validation_function(funcname, ast)
+    if %w(validate).include?funcname
+      if ast[2] and ast[2].type.to_s == "do_block"
+        if !(ast[2].source.include? "errors.add" or ast[2].source.include? "errors[")
+          found_missing($cur_app, $cur_file, ast.source)
+          # puts $cur_file
+          # puts "Missing error!"
+          # puts ast.source
+        else
+          did_not_find($cur_app, $cur_file)
+          # puts $cur_file
+          # puts "Error check - OK"
+        end
+      end
     end
   end
-
-end
-
-def check_validation_function(funcname, ast)
-  if ast.source.include? "errors.add"
-    puts "Found error message"
-  else
-    puts ast.source
-    puts "Did not find!"
+  if ast.type.to_s == "command"
+    funcname = ast[0].source
+    if %w(validate validates_each).include?funcname
+      if ast[2] and ast[2].type.to_s == "do_block"
+        if !(ast[2].source.include? "errors.add" or ast[2].source.include? "errors[")
+          found_missing($cur_app, $cur_file, ast.source)
+          # puts $cur_file
+          # puts "Missing error!"
+          # puts ast.source
+        else
+          did_not_find($cur_app, $cur_file)
+          # puts $cur_file
+          # puts "Error check - OK"
+        end
+      end
+    end
   end
+  if ast.type.to_s == "def" and funcnames.include? ast[0].source
+    if !(ast.source.include? "errors.add" or ast.source.include? "errors[")
+      found_missing($cur_app, $cur_file, ast.source)
+      # puts $cur_file
+      # puts "Missing error!"
+      # puts ast.source
+    else
+      did_not_find($cur_app, $cur_file)
+      # puts $cur_file
+      # puts "Error check - OK"
+    end
+  end
+end
+
+def search_ast_for_missing_errors(ast)
+  $func_name_acc = []
+  get_validation_function_names(ast)
+  funcnames = $func_name_acc.clone
+  check_for_error(ast, funcnames)
 
 end
 
-
-check_all_apps
+check_all_apps "/Users/utsavsethi/workspace/apps/"
+pp $error_results
