b3eb52dce417de7bbc268593a246496e1dfb2997
Utsav Sethi <utsavsethi@gmail.com>
2019-08-04 21:37:51 -0500

    Fix nested class declaration problem

--- constraint_analyzer/parse_model_constraint.rb
+++ constraint_analyzer/parse_model_constraint.rb
@@ -13,17 +13,20 @@ def parse_model_constraint_file(ast)
 	end
 	if ast.type.to_s == 'class'
 		c1 =  ast.children[0]
-		c2 = ast.children[1] 
-		if c1 and c1.type.to_s == 'const_ref'
+		c2 = ast.children[1]
+    if c2 and (c2.type.to_s == 'var_ref' or c2.type.to_s == 'const_path_ref')
+      if c2.source == 'RuntimeError' or c2.source == 'StandardError'
+        return
+      end
+      $cur_class.upper_class_name = c2.source
+    end
+    if c1 and c1.type.to_s == 'const_ref'
       puts "c1.source #{c1.source} class_name #{$cur_class.class_name}"
       if $cur_class.class_name
         $classes << $cur_class.dup
       end
 			$cur_class.class_name = c1.source
-		end
-		if c2 and (c2.type.to_s == 'var_ref' or c2.type.to_s == 'const_path_ref')
-			$cur_class.upper_class_name = c2.source
-		end
+    end
 		# puts"filename: #{$cur_class.filename} "
 		# puts"classname: #{$cur_class.class_name} upper_class_name: #{$cur_class.upper_class_name}"
 		c3 = ast.children[2]
